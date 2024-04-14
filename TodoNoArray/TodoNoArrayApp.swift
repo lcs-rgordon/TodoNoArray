de7eb09c7414dd0bdee361587599c4e9075651a2
Russell Gordon <rgordon@lcs.on.ca>
2024-04-13 20:13:06 -0400

    Initial Commit

---
+++ TodoNoArray/TodoNoArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoNoArrayApp.swift
+//  TodoNoArray
+//
+//  Created by Russell Gordon on 2024-04-13.
+//
+
+import SwiftUI
+
+@main
+struct TodoNoArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
